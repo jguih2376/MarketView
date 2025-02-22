78a52da9606417abce7f26543f63baf3e115075a
José Guilherme Mont. <jguih@live.com>
2025-02-22 00:41:06 -0300

    Add files via upload

--- pages/retorno_mensal.py
+++ pages/retorno_mensal.py
@@ -118,39 +118,66 @@ def app():
 
 #________________________________________________________________________________________________________________________________________________________
     st.markdown('---')
+
     # Função para carregar os dados usando yfinance
-    @st.cache_data
+    @st.cache_data(ttl=600)  # Cache atualizado a cada 10 min
     def carregar_dados(tickers, data_inicio, data_fim):
+        if not tickers:
+            return pd.DataFrame()
+        
         dados = {}
         for ticker in tickers:
             hist = yf.Ticker(ticker).history(start=data_inicio, end=data_fim)['Close']
             dados[ticker] = hist
-        return pd.DataFrame(dados)
+        
+        return pd.DataFrame(dados).dropna()  # Remove valores NaN
 
     def calcular_performance(dados):
-        # Função para calcular a performance em percentual
         if not dados.empty:
             return (dados / dados.iloc[0] - 1) * 100
         return dados
 
-    def criar_grafico(ativos_selecionados, dados):
+    def criar_grafico(ativos_selecionados, dados, normalizado=True, legenda_dict=None):
         fig = go.Figure()
         for ativo in ativos_selecionados:
+            nome_ativo = legenda_dict.get(ativo, ativo)  # Usa a chave do dicionário para o nome
+            # Dados normalizados ou brutos
+            y_data = calcular_performance(dados)[ativo] if normalizado else dados[ativo]
+            
+            # Adicionando linha do gráfico
             fig.add_trace(go.Scatter(
                 x=dados.index,
-                y=dados[ativo],
-                name=ativo,
+                y=y_data,
+                name=nome_ativo,  # Utiliza a chave do dicionário na legenda
+                mode='lines',  # Apenas a linha
                 line=dict(width=1)
             ))
+            
+            # Adicionando bolinha no último ponto
+            fig.add_trace(go.Scatter(
+                x=[dados.index[-1]],  # Último ponto do gráfico
+                y=[y_data.iloc[-1]],  # Último valor
+                mode='markers',
+                marker=dict(size=5, color='red', symbol='circle'),
+                name=f'{nome_ativo} - Último Preço',
+                showlegend=False
+            ))
 
+        # Ajustando a data do eixo X para intervalo de 1 ano
         fig.update_layout(
-            title='Desempenho Relativo dos Ativos (%)',
+            title=f"{'Desempenho Relativo (%)' if normalizado else 'Preço dos Ativos'}",
             xaxis_title='Data',
-            yaxis_title='Performance (%)',
-            xaxis=dict(tickformat='%m/%Y'),
-            legend_title='Ativo',
+            yaxis_title='Performance (%)' if normalizado else 'Preço',
+            xaxis=dict(
+                tickformat='%Y',  # Exibe o ano
+                tickmode='array',  # Define um modo de marcação personalizada
+                tickvals=dados.index[::252],  # Marca um ponto a cada 252 dias (aproximadamente 1 ano de pregão)
+            ),
+            legend_title='Ativos',
             legend_orientation='h',
-            plot_bgcolor='rgba(211, 211, 211, 0.15)'  # Cor de fundo cinza claro   
+            plot_bgcolor='rgba(211, 211, 211, 0.15)',
+            height=600,
+            margin=dict(r=10)  # Ajusta a margem à direita
         )
         fig.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot')
 
@@ -160,47 +187,58 @@ def app():
 
     opcao1 = st.radio('Selecione', ['Índices', 'Ações', 'Commodities'])
 
-    indices = {'IBOV': '^BVSP', 'S&P500': '^GSPC', 'NASDAQ': '^IXIC', 'FTSE100': '^FTSE', 'DAX': '^GDAXI', 
+    indices = {'IBOV': '^BVSP','EWZ':'EWZ', 'S&P500': '^GSPC', 'NASDAQ': '^IXIC', 'FTSE100': '^FTSE', 'DAX': '^GDAXI', 
             'CAC40': '^FCHI', 'SSE Composite': '000001.SS', 'Nikkei225': '^N225', 'Merval': '^MERV'}
 
     commodities = {'Ouro': 'GC=F', 'Prata': 'SI=F', 'Platina': 'PL=F', 'Cobre': 'HG=F', 'WTI Oil': 'CL=F', 
                 'Brent Oil': 'BZ=F', 'Milho': 'ZC=F', 'Soja': 'ZS=F', 'Café': 'KC=F'}
 
-    acoes = ['ALOS3', 'ABEV3', 'ASAI3', 'AURE3', 'AMOB3', 'AZUL4', 'AZZA3', 'B3SA3', 'BBSE3', 'BBDC3', 'BBDC4', 
-            'BRAP4', 'BBAS3', 'BRKM5', 'BRAV3', 'BRFS3', 'BPAC11', 'CXSE3', 'CRFB3', 'CCRO3', 'CMIG4', 'COGN3', 
-            'CPLE6', 'CSAN3', 'CPFE3', 'CMIN3', 'CVCB3', 'CYRE3', 'ELET3', 'ELET6', 'EMBR3', 'ENGI11', 'ENEV3', 
-            'EGIE3', 'EQTL3', 'FLRY3', 'GGBR4', 'GOAU4', 'NTCO3', 'HAPV3', 'HYPE3', 'IGTI11', 'IRBR3', 'ISAE4', 
-            'ITSA4', 'ITUB4', 'JBSS3', 'KLBN11', 'RENT3', 'LREN3', 'LWSA3', 'MGLU3', 'POMO4', 'MRFG3', 'BEEF3', 
-            'MRVE3', 'MULT3', 'PCAR3', 'PETR3', 'PETR4', 'RECV3', 'PRIO3', 'PETZ3', 'PSSA3', 'RADL3', 'RAIZ4', 
-            'RDOR3', 'RAIL3', 'SBSP3', 'SANB11', 'STBP3', 'SMTO3', 'CSNA3', 'SLCE3', 'SUZB3', 'TAEE11', 'VIVT3', 
-            'TIMS3', 'TOTS3', 'UGPA3', 'USIM5', 'VALE3', 'VAMO3', 'VBBR3', 'VIVA3', 'WEGE3', 'YDUQ3']
+    acoes = ["PETR4", "VALE3","ITUB4", "BBAS3", "BBDC4",
+            "RAIZ4","PRIO3", "VBBR3", "CSAN3", "UGPA3",  
+            "BPAC11", "SANB11", "GGBR4", "CSNA3", "USIM5",  
+            "JBSS3", "ABEV3", "MRFG3", "BRFS3", "BEEF3",  
+            "ELET3", "NEOE3", "CPFE3", "ENGI11", "EQTL3",  
+            "SUZB3", "KLBN11", "DTEX3", "RANI3", "MRFG3", 
+            "CYRE3", "MRVE3", "EZTC3", "CVCB3", "TRIS3", 
+            "WEGE3", "B3SA3"]
 
     acoes_dict = {acao: acao + '.SA' for acao in acoes}
-    col1,col2,col3=st.columns([4,1,1])
+
+    col1, col2, col3 = st.columns([3, 1, 1])
 
     with col1:
         if opcao1 == 'Índices':
             escolha = st.multiselect('Índice', list(indices.keys()), placeholder='Ativos')
             ticker = [indices[indice] for indice in escolha]
+            legenda_dict = {v: k for k, v in indices.items()}  # Inverte o dicionário para a legenda
 
         elif opcao1 == 'Commodities':
             escolha = st.multiselect('Commodities', list(commodities.keys()), placeholder='Ativos')
             ticker = [commodities[commodity] for commodity in escolha]
+            legenda_dict = {v: k for k, v in commodities.items()}  # Inverte o dicionário para a legenda
 
         elif opcao1 == 'Ações':
             escolha = st.multiselect('Ações', list(acoes_dict.keys()), placeholder='Ativos')
             ticker = [acoes_dict[acao] for acao in escolha]
+            legenda_dict = {v: k for k, v in acoes_dict.items()}  # Inverte o dicionário para a legenda
 
     with col2:
-        data_inicio = st.date_input('Data de início', pd.to_datetime('2015-01-01').date(), format='DD/MM/YYYY')
+        data_inicio = st.date_input('Data de início', pd.to_datetime('2020-01-01').date(), format='DD/MM/YYYY')
     with col3:
         data_fim = st.date_input('Data de término', pd.to_datetime('today').date(), format='DD/MM/YYYY')
 
+    # Opção para visualizar valores normalizados ou brutos
+    normalizado = st.checkbox("Exibir desempenho percentual", value=True)
+
     # Carregar os dados reais
-    dados = carregar_dados(ticker, data_inicio, data_fim)
-    if not dados.empty:
-        fig = criar_grafico(ticker, calcular_performance(dados))
-        st.plotly_chart(fig)
+    if ticker:
+        dados = carregar_dados(ticker, data_inicio, data_fim)
+        if not dados.empty:
+            fig = criar_grafico(ticker, dados, normalizado, legenda_dict)
+            st.plotly_chart(fig)
+        else:
+            st.warning("Nenhum dado disponível para os tickers selecionados.")
     else:
-        st.write("Nenhum dado disponível para os tickers selecionados.")
+        st.info("Selecione pelo menos um ativo para exibir os dados.")
+
 
