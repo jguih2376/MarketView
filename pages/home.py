2feacfbb9b368ae2a313cdd2b5bfb37348610ec0
José Guilherme Mont. <jguih@live.com>
2025-02-22 02:00:45 -0300

    Add files via upload

--- pages/home.py
+++ pages/home.py
@@ -8,18 +8,30 @@ def app():
     st.title("📊 Bem-vindo ao MarketView!")
 
     # Descrição do app
+    st.write('')
+    st.write('')
     st.write("""
-        O **MarketView** é a sua plataforma para acompanhar indicadores financeiros e econômicos em tempo real.  
+        O **MarketView** é a sua plataforma para acompanhar indicadores financeiros e econômicos.  
         Utilize o menu lateral para explorar as diversas páginas e funcionalidades do sistema, como:
-        - Calendário Econômico
-        - Panorama do Mercado
-        - Análise Histórica
+        - **Calendário Econômico**
+             
+        - **Panorama do Mercado**
+             
+        - **Análise Histórica**
              - Retorno Mensal
              - Desempenho Relativo
-        - Política Monetária
-        - Fundamentos de Ações
 
-        Escolha uma das opções acima para começar.
+        - **Política Monetária**
+             - Selic
+             - IPCA
+
+        - **Fundamentos de Ações**
+             - Dados Balanço Patrimonial
+             - Dados Domonstrativos de Resultados
+             - Indicadores Fundamentalista
+             - Performace entre ações
+
+        
     """)
 
 
