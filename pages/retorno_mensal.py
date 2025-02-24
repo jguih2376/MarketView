49f6f88d7945e45dd1b587c204e5b807bc563740
JGuih. <jguih@live.com>
2025-02-24 02:26:15 -0300

    Add files via upload

--- pages/retorno_mensal.py
+++ pages/retorno_mensal.py
@@ -204,6 +204,7 @@ def app():
                 tickformat='%b %Y',  # Exibe o ano
                 tickmode='array',  # Define um modo de marcação personalizada
                 tickvals=dados.index[::63],  # Marca um ponto a cada 252 dias (aproximadamente 1 ano de pregão)
+                tickangle=-45
             ),
             legend_title='Ativos',
             legend_orientation='h',
@@ -211,8 +212,8 @@ def app():
             height=600,
             margin=dict(r=10)  # Ajusta a margem à direita
         )
-        fig.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot')
-        fig.update_xaxes(showgrid=True, gridwidth=0.1, gridcolor='lightgray', griddash='dot')
+        fig.update_xaxes(showgrid=True, gridwidth=0.1, gridcolor='gray', griddash='dot')
+        fig.update_yaxes(showgrid=True, gridwidth=0.1, gridcolor='lightgray', griddash='dot')
 
         return fig
 
