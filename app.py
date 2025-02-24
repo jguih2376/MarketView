966025cbee6819dad99750c0116dc0b64261a03b
José Guilherme Mont. <jguih@live.com>
2025-02-23 21:18:14 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from pages import home, calendario_economico, panorama_do_mercado, retorno_mensal, politica_monetaria, fundamentos, criptos
+from pages import home, calendario_economico, panorama_do_mercado, retorno_mensal, politica_monetaria, fundamentos
 
 # Configuração global do Streamlit
 st.set_page_config(
@@ -25,7 +25,7 @@ if "page" not in st.session_state:
 st.sidebar.title("Navegação")
 pagina = st.sidebar.radio(
     "Escolha uma página", 
-    ["Home", "Calendário Econômico", "Panorama do Mercado", "Estatística Monetária", "Análise Histórica", "Fundamentos", "Criptos"]
+    ["Home", "Calendário Econômico", "Panorama do Mercado", "Estatística Monetária", "Análise Histórica", "Fundamentos"]
 )
 
 # Redirecionar para as páginas conforme a escolha do usuário
@@ -35,11 +35,10 @@ elif pagina == "Calendário Econômico":
     calendario_economico.app()
 elif pagina == "Panorama do Mercado":
     panorama_do_mercado.app()
-elif pagina == "Análise Histórica":
-    retorno_mensal.app()
 elif pagina == "Estatística Monetária":
     politica_monetaria.app()
+elif pagina == "Análise Histórica":
+    retorno_mensal.app()
 elif pagina == "Fundamentos":
     fundamentos.app()
-elif pagina == "Criptos":
-    criptos.app()
+
